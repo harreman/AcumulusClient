b305dd519853c17c1d102c79e458a9785883cb85
Sander Harreman <sharreman@harreman.it>
2017-05-06 09:48:43 +0200

    initial

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AcumulusClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Client for acumulus")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Harreman.IT")]
 [assembly: AssemblyProduct("AcumulusClient")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bf8e3ed1-63db-4bd3-a5c9-3550bb34c5d7")]
